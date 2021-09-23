00f26e7113e99f0bee6b8032677b39524f4db381
almondjain <49396182+almondjain@users.noreply.github.com>
2021-09-23 12:15:31 +0530

    set css part

--- script.js
+++ script.js
@@ -3,16 +3,16 @@ const luckyNumber = document.querySelector("#lucky-number");
 const checkButton = document.querySelector("#number-calculate");
 const displayMessage = document.querySelector("#output-container");
 
-function calculateDobSum(dob){
-    let sum=0;
-    dob = dob.replaceAll("-","");
-    for(let digit of dob){
-        sum+= Number(digit);
+function calculateDobSum(dob) {
+    let sum = 0;
+    dob = dob.replaceAll("-", "");
+    for (let digit of dob) {
+        sum += Number(digit);
     }
     return sum;
 }
 
-function showMessage(message){
+function showMessage(message) {
     displayMessage.innerText = message;
 }
 
@@ -20,17 +20,16 @@ checkButton.addEventListener("click", () => {
     const dob = dateOfBirth.value;
     const noToCheck = luckyNumber.value;
 
-    if(dob && noToCheck)
-    {
+    if (dob && noToCheck) {
         const sumOfDob = calculateDobSum(dob);
-        console.log('summm'+sumOfDob);
+        console.log('summm' + sumOfDob);
         //checking no.is lucky or not!
-        if(sumOfDob % noToCheck === 0){
-            showMessage("Your Date of Birth {dob} is lucky!");
-        }else{
+        if (sumOfDob % noToCheck === 0) {
+            showMessage("Your Date of Birth is lucky!");
+        } else {
             showMessage("Oops! Your Date of Birth is not lucky!");
         }
-    }else{
+    } else {
         showMessage("Please Enter both fields!");
     }
 });
